3931e2742b2afc87c28daf0478b5163460dbe05b
Ivan Doroshenko <idorosenko24@gmail.com>
2024-02-01 14:06:56 +0200

    delete sayBy

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const a = 2;
 const b = 3;
+
 function sayHi() {
      alert('Hello world');
 };
@@ -7,10 +8,7 @@ function sayHi() {
 function work() {
      alert('Мотивацию нужно ПОДНЯЯЯЯТЬ!!!');
 };
-// dfsgdsghhgfh
-function sayBye() {
-     alert('Goodbye World');
-};
+
 
 function calculation(a,b) {
      let calculate = a + b;
